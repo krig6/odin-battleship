43cb7e386594d93893318ef1580ba4d342d97c4f
krig6 <ej.ldsm@gmail.com>
2025-07-08 17:35:05 +0800

    feat(player): Implement Gameboard instance to each player
    - Imported `Gameboard` and initialized a new instance inside the
    `Player` constructor
    - Ensures that each `Player` object contains its own `Gameboard`
    - Test in the previous commit for `Gameboard` assignment now passes

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,10 @@
+import Gameboard from '../src/Gameboard.js';
+
 class Player {
   constructor(name = 'Nameless Admiral', isComputer = false) {
     this.name = name;
     this.isComputer = isComputer;
+    this.board = new Gameboard();
   }
 }
 
