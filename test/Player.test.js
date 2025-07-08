366752c77b8bde8e4a5fd34a87bd3b3ecee7748b
krig6 <ej.ldsm@gmail.com>
2025-07-08 17:24:16 +0800

    test(player): Add failing tests for isComputer flag
    - Added test to verify that a player can be identified as a computer
    when `isComputer` is set to true
    - Added test to verify that `isComputer` defaults to false when not
    specified

--- test/Player.test.js
+++ test/Player.test.js
@@ -4,3 +4,13 @@ it('creates a Player instance', () => {
   const playerOne = new Player();
   expect(playerOne.name).toBe('Nameless Admiral');
 });
+
+it('can identify if the player is a computer', () => {
+  const player = new Player('Bot', true);
+  expect(player.isComputer).toBe(true);
+});
+
+it('defaults isComputer to false', () => {
+  const player = new Player();
+  expect(player.isComputer).toBe(false);
+});
