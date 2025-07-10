a62a8b53a1569946ff0bfc863c7a77575b439d6d
krig6 <ej.ldsm@gmail.com>
2025-07-10 21:53:33 +0800

    feat(game-controller): Implement temporary fleet placement logic
    - Implemented `initialFleetPlacement` to place player1's fleet on the
    board at predetermined coordinates
    - This is a temporary solution to allow game interaction while core
    machanics are being developed

--- src/gameController.js
+++ src/gameController.js
@@ -1,5 +1,6 @@
 import Player from './Player.js';
 import Ship from './Ship.js';
+import { updatePlayerGameBoard } from './domController.js';
 const player1 = new Player();
 const player2 = new Player('Computer', true);
 const player1Board = document.getElementById('player-one-board');
@@ -17,5 +18,18 @@ const createFleet = (fleetData = FLEET_CONFIG) => {
   fleetData.forEach(({ type, length }) => {
     fleet[type] = new Ship(type, length);
   });
+};
+
+const initialFleetPlacement = () => {
+  const fleet = player1.gameboard.fleet;
+  let row = 0;
+
+  for (const type in fleet) {
+    const ship = fleet[type];
+    player1.gameboard.placeShip(row, 0, ship);
+    row++;
+  }
+
+  updatePlayerGameBoard(player1, player1Board);
 };
 
