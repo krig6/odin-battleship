98f2190441b507a6f4538ee7b27bb67f420e3d4d
krig6 <ej.ldsm@gmail.com>
2025-07-10 21:26:35 +0800

    feat(game-controller): Initialize players and boards(
    - Create `gameboardController.js` to encapsulate game setup logic
    - Initializes two players: `player1` and `player2`
    - Each player is assigned a new instance of `Gameboard`

---
+++ src/gameController.js
@@ -0,0 +1,5 @@
+import Player from './Player.js';
+const player1 = new Player();
+const player2 = new Player('Computer', true);
+const player1Board = document.getElementById('player-one-board');
+const player2Board = document.getElementById('player-two-board');
