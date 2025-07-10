02ebd894fa5deb170cc0a3398c2b0e382592ca5f
krig6 <ej.ldsm@gmail.com>
2025-07-10 21:31:13 +0800

    feat(game-controller): Define default fleet configuration
    - Added `FLEET_CONFIG` array containing default ship types
    - To be used in creating a standard fleet for each player during game
    setup
    - Improves maintainability by centralizing ship data in one place,
    making it easier to update or customize fleet definitions

--- src/gameController.js
+++ src/gameController.js
@@ -3,3 +3,10 @@ const player1 = new Player();
 const player2 = new Player('Computer', true);
 const player1Board = document.getElementById('player-one-board');
 const player2Board = document.getElementById('player-two-board');
+const FLEET_CONFIG = [
+  { type: 'carrier', length: 5 },
+  { type: 'battleship', length: 4 },
+  { type: 'destroyer', length: 3 },
+  { type: 'submarine', length: 2 },
+  { type: 'patrolBoat', length: 1 }
+];
