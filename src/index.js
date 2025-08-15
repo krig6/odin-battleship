cdfa27a05e2c79448c580246ee20884feaab4825
krig6 <ej.ldsm@gmail.com>
2025-08-15 19:52:44 +0800

    feat(footer): Add footer component and style
    - Created new file `footer.js` for rendering footer dynamically
    - Added styles for `site-footer` and `site-footer__icon`
    - Imported and invoked `renderFooter`in `index.js`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
 import { setupPlayerOnePlacementScreen } from './controllers/gameController.js';
+import { renderFooter } from './components/footer.js';
 
-setupPlayerOnePlacementScreen(); 
+setupPlayerOnePlacementScreen();
+renderFooter();
